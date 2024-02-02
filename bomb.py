7a3da5c55009a82f949bbac462e1566101eebbd5
Anonymous Hacker <158556461+ShieldMaster1@users.noreply.github.com>
2024-02-02 14:36:12 +0530

    bomb.py

---
+++ bomb.py
@@ -0,0 +1,28 @@
+import time
+import webbrowser
+
+# Color codes
+GREEN = '\033[0;32m'
+NC = '\033[31m\a\a'  # No Color
+
+print(f"{GREEN}\n"
+      f"$@#      @#$$@#      @#$    @#$@#$@#$@#$"
+      f" @#$    @#$ @#$    @#$      @#        #$"
+      f"  @#$  @#$   @#$  @#$       @#$@#$@#$@#$"
+      f"   @#$@#$     @#$@#$        @#"
+      f"    @#$        @#$          @#"
+      f"            \n"
+      f"{NC}")
+
+print("Whatsapp Bomb Messages Simple Tool")
+print("Author: ./Cyber_Dila\n")
+
+c = input("Enter Victim Country Code (Without +): ")
+num = input("Enter Victim Phone Number (Without Country Code): ")
+text = input("Enter Your Message: ")
+
+print("Starting Program...")
+time.sleep(2)
+
+# Open the YouTube channel in the default web browser
+webbrowser.open(f"https://wa.me/{c}{num}/?text={text}")
